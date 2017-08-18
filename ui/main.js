a6d193963a6fde08cbb7cbb0d2c01aecff2d8be8
chandu sriharsha <chandu.harsha111@gmail.com>
2017-08-19 01:51:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,12 +7,12 @@ element.innerHTML='Hi this is Harsha from St.peters Engineering College';
 var img=document.getElementById('madi');
 var marginLeft=0;
  function moveRight(){
-     marginLeft=marginLeft+10;
+     marginLeft=marginLeft+1;
      img.style.marginLeft=marginLeft+'px';
  }
  
  
  img.onclick=function(){
      var interval=setInterval(moveRight,50);
-     img.style.marginLeft='100px';
+     img.style.marginLeft='50px';
  };
