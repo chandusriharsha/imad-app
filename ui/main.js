d1a396b7d6ba093506bc004fa923eb9fef623446
chandu sriharsha <chandu.harsha111@gmail.com>
2017-08-19 01:27:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //change the text of main-text
-var element=document.getElementbyid('main-text');
+var element=document.getElementbyid('main-text')
 element.innerHTML='Hi this is Harsha from St.peters Engineering College';
