542c30f68398be0f6261232a42f9808f66dc5e0f
chandu sriharsha <chandu.harsha111@gmail.com>
2017-08-19 02:05:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/SRIHARSHA.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'SRIHARSHA.png'));
 });
 
